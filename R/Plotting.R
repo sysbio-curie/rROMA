6f9435b5b602e1b1811855ac72f05627cad8c5f7
njmmatthieu <26273598+njmmatthieu@users.noreply.github.com>
2022-07-30 00:00:29 +0200

    pb version roma

--- R/Plotting.R
+++ R/Plotting.R
@@ -84,6 +84,7 @@ Plot.Genesets.Samples <- function(RomaData, Selected = NULL,
   }
   
   if(length(Selected) < 2){
+    print('hello')
     print("Heatmap is not clustering module activities when less than 2 modules are selected")
   }
   if((length(ColorGradient) %% 2) != 0){
